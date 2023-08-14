bd0cc72791d7b58ea1580265dc4ddee376d78144
Devil X - Sameer Khan <116139678+devilxsam@users.noreply.github.com>
2023-08-15 00:45:12 +0530

    Update settings.py

--- sources/settings.py
+++ sources/settings.py
@@ -35,10 +35,10 @@ SETTINGS = {
     "CAPTCHABOT_PRIVATE": False,
 
     # Bot Token (get it from @BotFather)
-    "CAPTCHABOT_TOKEN": "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "CAPTCHABOT_TOKEN": "6542713865:AAG1-octzYBUydv7NT1IpP62HXAyUf8gOhY",
 
     # Bot Owner (i.e. "@JoseTLG" or "123456789")
-    "CAPTCHABOT_OWNER": "XXXXXXXXX",
+    "CAPTCHABOT_OWNER": "5795704298",
 
     # Bot Webhook Host addres (keep in None for Polling or set to a
     # valid address for Webhook)
